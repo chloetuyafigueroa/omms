3ee79b6a86f6aa6a05d190f9ebd2371c29252772
EC-FIGUEROA <figueroaec@gmail.com>
2026-06-25 08:58:21 +0800

    update

--- src/jspbook/tsd/DatabaseConnection.java
+++ src/jspbook/tsd/DatabaseConnection.java
@@ -41,8 +41,8 @@ public class DatabaseConnection implements ServletContextListener {
 
                 // Establish the connection
                 //String url = "jdbc:postgresql://172.17.100.6:5432/joblist";
-                String url = "jdbc:postgresql://postgres:03_0431Achloe@db.pfonglwyrcfiigivcpoj.supabase.co:5432/postgres/ommscloud";
-                String username = "postgres";
+                String url = "jdbc:postgresql://db.pfonglwyrcfiigivcpoj.supabase.co:5432/postgres?sslmode=require";
+String username = "postgres";
                 String password = "03_0431Achloe";//ileco1_amfm
                 connection = DriverManager.getConnection(url, username, password);
             } catch (ClassNotFoundException e) {
