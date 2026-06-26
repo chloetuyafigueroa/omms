f4f50bd5e003a9d486f9927ab0193a8e9c5ba03b
EC-FIGUEROA <figueroaec@gmail.com>
2026-06-26 16:17:16 +0800

    update

--- src/jspbook/tsd/ConfigServlet.java
+++ src/jspbook/tsd/ConfigServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/config.js")
 public class ConfigServlet extends HttpServlet {
 
